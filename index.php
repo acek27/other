73d2fe42277dc36717be478d49b5e3223b01303b
razak syaiful rochman <razak.syaiful.r@gmail.com>
2018-09-03 14:05:43 +0700

    edit

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>HBD | Devi Putri</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/icon" href="assets/images/favicon.jpg"/>
+    <!-- Font Awesome -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Slick slider -->
+    <link href="assets/css/slick.css" rel="stylesheet">
+    <!-- Theme color -->
+    <link id="switcher" href="assets/css/theme-color/default-theme.css" rel="stylesheet">
+
+    <!-- Main Style -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- Fonts -->
+
+    <!-- Open Sans for body font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700,800" rel="stylesheet">
+	<!-- Montserrat for title -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+ 
+ 
+	
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  	
+  	<!-- Start Header -->
+	<header id="mu-hero" class="" role="banner">
+		<!-- Start menu  -->
+		<nav class="navbar navbar-fixed-top navbar-default mu-navbar">
+		  	<div class="container">
+			    <!-- Brand and toggle get grouped for better mobile display -->
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+
+			      <!-- Logo -->
+			      <a class="navbar-brand" href="index.html">Qiana</a>
+
+			    </div>
+
+			    <!-- Collect the nav links, forms, and other content for toggling -->
+			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			      	<ul class="nav navbar-nav mu-menu navbar-right">
+			      		<li><a href="#mu-hero">Home</a></li>
+				        <li><a href="#mu-about">Tahukah kamu?</a></li>
+			            <li><a href="#mu-register">Ucapan</a></li>
+			            <li><a href="#mu-contact">Tentang Kami</a></li>
+			      	</ul>
+			    </div><!-- /.navbar-collapse -->
+		  	</div><!-- /.container-fluid -->
+		</nav>
+		<!-- End menu -->
+
+		<div class="mu-hero-overlay">
+			<div class="container">
+				<div class="mu-hero-area">
+
+					<!-- Start hero featured area -->
+					<div class="mu-hero-featured-area">
+
+						<div class="mu-hero-featured-content">
+
+							<h1>HELLO! SELAMAT DATANG DEVI PUTRI</h1>
+							<h2>Manusia yang suka ngeluh kalo lagi	 kuliah!</h2>
+							<p class="mu-event-date-line" id="date"></p>
+
+							<div class="mu-event-counter-area">
+							<img src="assets/images/text.png" alt="speaker img">						
+								</div>
+							</div>
+
+						</div>
+					</div>
+					<!-- End hero featured area -->
+
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- End Header -->
+	
+	<!-- Start main content -->
+	<main role="main">
+		<!-- Start About -->
+		<section id="mu-about">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mu-about-area">
+							<!-- Start Feature Content -->
+							<div class="row">
+								<div class="col-md-6">
+									<div class="mu-about-left">
+										<img class="" src="assets/images/about.jpg" alt="Men Speaker">
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="mu-about-right">
+										<h2>Tahukah kamu?</h2>
+										<p>Devi Putri</p>
+
+										<p>adalah sesosok manusia jadi-jadian.. eh salah,</p>
+
+										<p>Dia adalah seseorang yang spesial dihati penggemarnya. menurut rumor yang beredar, dia tipe wanita yang sadis, cuek, pendiam, menyeramkan dan sejenis itu lah. Akan tetapi, admin tidak percaya dengan rumor tersebut. sebab dengan kita melihat raut wajahnya saja, akan banyak pria yang tergila-gila padanya. sepertinya Admin naksir nih sama dia hahaha (si Admin mulai gak jelas). Kabar terbarunya nih, dia uda siap jadi perawat nih. tetap semangat yaaa, semoga diberi kesehatan biar tetap bisa membantu orang-orang, merawat pasien dan juga Admin. eeem, Saat ini nih, hari ini tepatnya 03 September 2018 tepat hari ulang tahunnya yang insyaallah adminnya lola kalo ngitung, dia sudah menginjak usia 22 tahun. benar tidak ya? anggap aja benar ehehe. yuk, kita ucapin selamat ulang tahun sama-sama :)</p>
+									</div>
+								</div>
+							</div>
+							<!-- End Feature Content -->
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End About -->
+
+		<!-- Start Video -->
+		<section id="mu-video">
+			<div class="mu-video-overlay">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="mu-video-area">
+								<h2>Yuk tonton video sebelumnya..</h2>
+								<p>Maaf, Admin belum sempat bikin video terbaru</p>
+								<a class="mu-video-play-btn" href="#"><i class="fa fa-play" aria-hidden="true"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Start Video content -->
+			<div class="mu-video-content">
+				<div class="mu-video-iframe-area">
+					<a class="mu-video-close-btn" href="#"><i class="fa fa-times" aria-hidden="true"></i></a>
+					<iframe width="854" height="480" src="https://www.youtube.com/embed/iJMlrTSqKKs" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+			<!-- End Video content -->
+
+		</section>
+		<!-- End Video -->
+		<!-- Start Register  -->
+		<section id="mu-register">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mu-register-area">
+
+							<div class="mu-title-area">
+								<h2 class="mu-title">Ucapan Ulang Tahun</h2>
+								<p>Dear Devi Putri,</p>
+								<p>Selamat ulang tahun yaa.. Semoga apa yang kamu cita-citakan bisa tercapai. Semoga dengan bertambahnya usiamu, semakin sempurna juga iman serta akhlakmu. Tetaplah menjadi wanita yang baik. Taat dan patuh terhadap orang tua. Ramah terhadap siapapun dan selalu awali harimu dengan senyuman yang indah. Semoga Allah akan selalu menyayangi dan melindungi di setiap langkahmu.</p>
+								<p>Tiada kado yang paling istimewa selain doa, semoga kamu selalu bahagia. Eeemm, sehat selalu yaa.. :)</p>
+							</div>
+							<div class="mu-event-counter-area">
+							<img src="assets/images/textucapan2.gif" style="display: block;margin-left: auto;margin-right: auto;" alt="speaker img">						
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Register -->
+
+		<!-- Start Contact -->
+		<section id="mu-contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mu-contact-area">
+
+							<div class="mu-title-area">
+								<h2 class="mu-heading-title">Tentang Kami</h2>
+								<p>Qiana Developers, Make Your Life Better...</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Contact -->
+
+	</main>
+	
+	<!-- End main content -->	
+			
+			
+	<!-- Start footer -->
+	<footer id="mu-footer" role="contentinfo">
+			<div class="container">
+				<div class="mu-footer-area">
+					<div class="mu-footer-top">
+						<div class="mu-social-media">
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-youtube"></i></a>
+						</div>
+					</div>
+					<div class="mu-footer-bottom">
+						<p class="mu-copy-right">&copy; Copyright <a rel="nofollow" href="https://www.instagram.com/cek.acek/">Razak Syaiful Rochman</a>. All right reserved.</p>
+					</div>
+				</div>
+			</div>
+
+	</footer>
+	<!-- End footer -->
+
+	
+	<script>
+	n =  new Date();
+	var month = new Array();
+	month[0] = "January";
+	month[1] = "February";
+	month[2] = "March";
+	month[3] = "April";
+	month[4] = "May";
+	month[5] = "June";
+	month[6] = "July";
+	month[7] = "August";
+	month[8] = "September";
+	month[9] = "October";
+	month[10] = "November";
+	month[11] = "December";
+	
+	y = n.getFullYear();
+	m = month[n.getMonth()];
+	d = n.getDate();
+	document.getElementById("date").innerHTML = d + " / " + m + " / " + y;
+	</script>
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Bootstrap -->
+    <script src="assets/js/bootstrap.min.js"></script>
+	<!-- Slick slider -->
+    <script type="text/javascript" src="assets/js/slick.min.js"></script>
+    <!-- Event Counter -->
+    <script type="text/javascript" src="assets/js/jquery.countdown.min.js"></script>
+    <!-- Ajax contact form  -->
+    <script type="text/javascript" src="assets/js/app.js"></script>
+  
+       
+	
+    <!-- Custom js -->
+	<script type="text/javascript" src="assets/js/custom.js"></script>
+
+	
+	
+    
+  </body>
+</html>
